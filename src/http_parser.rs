c72fa43ad18baa503e1df2b6c463328cf6a8737a
GenYuLi <hank90555@gmail.com>
2024-08-27 23:49:40 +0800

    pass stage

---
+++ src/http_parser.rs
@@ -0,0 +1,43 @@
+use std::error::Error;
+pub(crate) struct HttpRequest {
+    // Use pub for debugging purposes
+    pub method: String,
+    pub request_uri: String,
+    pub http_version: String,
+    pub headers: Vec<(String, String)>,
+    pub body: String,
+}
+
+impl HttpRequest {
+    pub fn new() -> HttpRequest {
+        HttpRequest {
+            method: String::new(),
+            request_uri: String::new(),
+            http_version: String::new(),
+            headers: Vec::new(),
+            body: String::new(),
+        }
+    }
+
+    pub fn parse_request(&mut self, request: &str) -> Result<(), Box<dyn Error>> {
+        let mut lines = request.lines();
+        let request_line = lines.next().ok_or("no request line")?;
+        println!("{}", request_line);
+        let mut parts = request_line.split_whitespace();
+        self.method = parts.next().ok_or("method error")?.to_string();
+        self.request_uri = parts.next().ok_or("url error")?.to_string();
+        self.http_version = parts.next().ok_or("version error{}")?.to_string();
+
+        match self.request_uri.as_str() {
+            "/" => {
+                self.body = "Hello, world!".to_string();
+            }
+            _ => {
+                return Err("404 not found".into());
+            }
+        }
+
+        // TODO: Parse headers and body
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::error::Error;
+mod http_parser;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,11 +17,24 @@ fn main() {
                 println!("accepted new connection");
                 let mut buf: [u8; 1024] = [0; 1024];
                 _stream.read(&mut buf).unwrap();
-                _stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                let s = String::from_utf8_lossy(&buf);
+                let mut req = http_parser::HttpRequest::new();
+                let result = req.parse_request(&s);
+                match result {
+                    Ok(()) => {
+                        _stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                    }
+                    Err(e) => {
+                        _stream.write("HTTP/1.1 400 Bad Request\r\n\r\n".as_bytes()).unwrap();
+                    }
+                }
+
+
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
 }
